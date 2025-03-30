dee6cd669cb0bedaad6be443c5eaea1594c9d58c
Pavel5807 <5807keam.hi@gmail.com>
2025-03-30 13:06:47 +0300

    feat: added graphql

--- src/Tasks/Tasks.API/Program.cs
+++ src/Tasks/Tasks.API/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Tasks.API.Application.Models;
+using TaskFlow.Tasks.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDefaultAuthentication(builder.Configuration);
 builder.Services.AddAuthorization();
+
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>()
+    .AddFiltering();
 
 builder.Services.AddDefaultOpenApi(builder.Configuration);
 
@@ -14,6 +23,19 @@ var app = builder.Build();
 app.UseOpenApi(builder.Configuration);
 app.UseAuthentication();
 app.UseAuthorization();
+app.MapGraphQL();
 app.MapControllers();
 
 app.Run();
+
+public class Query
+{
+    [UseFiltering]
+    public IQueryable<TaskDTO> GetTasks(TaskDbContext context) 
+    {
+        return context.Tasks
+            .Include(task => task.History)
+            .Include(task => task.Items)
+            .Select(task => task.ToDTO());
+    }
+}
